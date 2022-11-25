0e58c3ef07b366f3011dcb6ecc0b45153deaa5f9
7N <cce350276@gmail.com>
2022-11-25 09:26:35 +0800

    page2 +page 4 html

--- src/app/page2-week-flow/page2-week-flow.component.ts
+++ src/app/page2-week-flow/page2-week-flow.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-page2-week-flow',
   templateUrl: './page2-week-flow.component.html',
   styleUrls: ['./page2-week-flow.component.scss']
 })
-export class Page2WeekFlowComponent {
+export class Page2WeekFlowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  get isOneAvailable(): boolean {
+    return this.one && this.one.length < 1;
+}
+
+  OnePredicate = (): boolean => {
+    return this.isOneAvailable;
+}
+
+get isTwoAvailable(): boolean {
+  return this.two && this.two.length < 1;
+}
+
+TwoPredicate = (): boolean => {
+  return this.isTwoAvailable;
+}
+
+AllIn:string[] = ['A', 'B'];
+
+one:string[] = [];
+
+two:string[] = [];
+
+
+
+
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+  visinfo(){
+
+
+    if(this.one[0]=="A"&&this.two[0]=="B"){
+      alert("true");
+
+    }else{
+  
+      alert("false");
+    }
+  
+  }
 
 }
